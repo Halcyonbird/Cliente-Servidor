eff8825f0499c0e4476535eb5e4712a72360c856
Halcyonbird <cesarbeliever@gmail.com>
2019-08-30 16:07:18 -0500

    Initial commit

---
+++ src/cliente/servidor/MainCliente.java
@@ -0,0 +1,20 @@
+
+package cliente.servidor;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Hawer Forero
+ */
+public class MainCliente {
+    
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+    
+}
---
+++ src/cliente/servidor/MainServidor.java
@@ -0,0 +1,18 @@
+
+package cliente.servidor;
+
+import java.io.IOException;
+/**
+ *
+ * @author Hawer Forero
+ */
+public class MainServidor {
+
+    public static void main(String[] args) throws IOException
+    {
+        Servidor serv = new Servidor(); //Se crea el servidor
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+}
